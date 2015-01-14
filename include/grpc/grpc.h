e12b266f5211a62ba253c7d03731c0931cc797ce
Sunriver <seriousman1994217winter@gmail.com>
2015-01-13 16:56:18 -0800

    Don't mix C and C++ libraries.

--- include/grpc/grpc.h
+++ include/grpc/grpc.h
@@ -158,7 +158,6 @@ typedef struct grpc_byte_buffer grpc_byte_buffer;
 
 /* Sample helpers to obtain byte buffers (these will certainly move place */
 grpc_byte_buffer *grpc_byte_buffer_create(gpr_slice *slices, size_t nslices);
-grpc_byte_buffer *grpc_byte_buffer_copy(grpc_byte_buffer *bb);
 size_t grpc_byte_buffer_length(grpc_byte_buffer *bb);
 void grpc_byte_buffer_destroy(grpc_byte_buffer *byte_buffer);
 
@@ -320,9 +319,6 @@ grpc_call_error grpc_call_add_metadata(grpc_call *call, grpc_metadata *metadata,
    Produces a GRPC_FINISHED event with finished_tag when the call has been
        completed (there may be other events for the call pending at this
        time) */
-grpc_call_error grpc_call_invoke(grpc_call *call, grpc_completion_queue *cq,
-                                 void *metadata_read_tag, void *finished_tag,
-                                 gpr_uint32 flags);
 grpc_call_error grpc_call_start_invoke(grpc_call *call,
                                        grpc_completion_queue *cq,
                                        void *invoke_accepted_tag,
