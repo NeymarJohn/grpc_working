3c9606ce6bf4b2c1c94f463156354bb84d456509
Sunriver <seriousman1994217winter@gmail.com>
2015-01-15 07:39:09 -0800

    Allow language to be specified to run_tests.py

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -40,7 +40,7 @@ _CONFIGS = {
   'msan': SimpleConfig('msan'),
   'asan': SimpleConfig('asan'),
   'gcov': SimpleConfig('gcov'),
-  'memcheck': ValgrindConfig('valgrind', 'memcheck'),
+  'memcheck': ValgrindConfig('dbg', 'memcheck'),
   'helgrind': ValgrindConfig('dbg', 'helgrind')
   }
 
