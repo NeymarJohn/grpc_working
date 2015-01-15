6f517a3ca761febe35b1fbfd5bd6ed4f2df3a78b
Sunriver <seriousman1994217winter@gmail.com>
2015-01-14 16:57:24 -0800

    Various cosmetic improvements to run_tests.py

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -36,6 +36,7 @@ _CONFIGS = {
   'tsan': SimpleConfig('tsan'),
   'msan': SimpleConfig('msan'),
   'asan': SimpleConfig('asan'),
+  'gcov': SimpleConfig('gcov'),
   'valgrind': ValgrindConfig('dbg'),
   }
 
@@ -55,6 +56,10 @@ argp.add_argument('-f', '--forever',
                   default=False,
                   action='store_const',
                   const=True)
+argp.add_argument('--newline_on_success',
+                  default=False,
+                  action='store_const',
+                  const=True)
 args = argp.parse_args()
 
 # grab config
@@ -68,38 +73,48 @@ runs_per_test = args.runs_per_test
 forever = args.forever
 
 
-def _build_and_run(check_cancelled):
+def _build_and_run(check_cancelled, newline_on_success, forever=False):
   """Do one pass of building & running tests."""
   # build latest, sharing cpu between the various makes
   if not jobset.run(
       (['make',
         '-j', '%d' % (multiprocessing.cpu_count() + 1),
-        target,
-        'CONFIG=%s' % cfg]
-       for cfg in build_configs
-       for target in _MAKE_TEST_TARGETS),
+        'CONFIG=%s' % cfg] + _MAKE_TEST_TARGETS
+       for cfg in build_configs),
       check_cancelled, maxjobs=1):
-    sys.exit(1)
+    return 1
 
   # run all the tests
-  jobset.run((
+  if not jobset.run((
       config.run_command(x)
       for config in run_configs
       for filt in filters
       for x in itertools.chain.from_iterable(itertools.repeat(
           glob.glob('bins/%s/%s_test' % (
               config.build_config, filt)),
-          runs_per_test))), check_cancelled)
+          runs_per_test))), check_cancelled, newline_on_success=newline_on_success):
+    if not forever:
+      jobset.message('FAILED', 'Some tests failed', do_newline=True)
+    return 2
+
+  if not forever:
+    jobset.message('SUCCESS', 'All tests passed', do_newline=True)
+  return 0
 
 
 if forever:
+  success = True
   while True:
     dw = watch_dirs.DirWatcher(['src', 'include', 'test'])
     initial_time = dw.most_recent_change()
     have_files_changed = lambda: dw.most_recent_change() != initial_time
-    _build_and_run(have_files_changed)
+    previous_success = success
+    success = _build_and_run(have_files_changed, newline_on_success=False, forever=True) == 0
+    if not previous_success and success:
+      jobset.message('SUCCESS', 'All tests are now passing properly', do_newline=True)
+    jobset.message('IDLE', 'No change detected')
     while not have_files_changed():
       time.sleep(1)
 else:
-  _build_and_run(lambda: False)
+  sys.exit(_build_and_run(lambda: False, newline_on_success=args.newline_on_success))
 
