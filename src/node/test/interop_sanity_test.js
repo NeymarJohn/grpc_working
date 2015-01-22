608f62bf51126ff9dbdeda367fe73a215f0b8981
Sunriver <seriousman1994217winter@gmail.com>
2015-01-21 18:17:25 -0800

    Updates the Java Dockerfiles to pull source from GitHub

--- src/node/test/interop_sanity_test.js
+++ src/node/test/interop_sanity_test.js
@@ -48,9 +48,6 @@ describe('Interop tests', function() {
     port = 'localhost:' + server_obj.port;
     done();
   });
-  after(function() {
-    server.shutdown();
-  });
   // This depends on not using a binary stream
   it('should pass empty_unary', function(done) {
     interop_client.runTest(port, name_override, 'empty_unary', true, done);
@@ -68,7 +65,7 @@ describe('Interop tests', function() {
   it('should pass ping_pong', function(done) {
     interop_client.runTest(port, name_override, 'ping_pong', true, done);
   });
-  it('should pass empty_stream', function(done) {
+  it.skip('should pass empty_stream', function(done) {
     interop_client.runTest(port, name_override, 'empty_stream', true, done);
   });
 });
