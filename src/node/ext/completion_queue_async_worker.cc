35a1c2d0002fad2f96823e77eef3cd9a35d4b5a8
Sunriver <seriousman1994217winter@gmail.com>
2015-02-11 16:23:01 -0800

    Fix hash table

--- src/node/ext/completion_queue_async_worker.cc
+++ src/node/ext/completion_queue_async_worker.cc
@@ -35,10 +35,10 @@
 #include <nan.h>
 
 #include "grpc/grpc.h"
-#include "grpc/support/log.h"
 #include "grpc/support/time.h"
 #include "completion_queue_async_worker.h"
-#include "call.h"
+#include "event.h"
+#include "tag.h"
 
 namespace grpc {
 namespace node {
@@ -58,10 +58,6 @@ CompletionQueueAsyncWorker::~CompletionQueueAsyncWorker() {}
 
 void CompletionQueueAsyncWorker::Execute() {
   result = grpc_completion_queue_next(queue, gpr_inf_future);
-  gpr_log(GPR_DEBUG, "Handling response on call %p", result->call);
-  if (result->data.op_complete != GRPC_OP_OK) {
-    SetErrorMessage("The batch encountered an error");
-  }
 }
 
 grpc_completion_queue *CompletionQueueAsyncWorker::GetQueue() { return queue; }
@@ -79,27 +75,14 @@ void CompletionQueueAsyncWorker::Init(Handle<Object> exports) {
 
 void CompletionQueueAsyncWorker::HandleOKCallback() {
   NanScope();
-  gpr_log(GPR_DEBUG, "Handling response on call %p", result->call);
-  NanCallback *callback = GetTagCallback(result->tag);
-  Handle<Value> argv[] = {NanNull(), GetTagNodeValue(result->tag)};
-
-  callback->Call(2, argv);
+  NanCallback event_callback(GetTagHandle(result->tag).As<Function>());
+  Handle<Value> argv[] = {CreateEventObject(result)};
 
   DestroyTag(result->tag);
   grpc_event_finish(result);
   result = NULL;
-}
 
-void CompletionQueueAsyncWorker::HandleErrorCallback() {
-  NanScope();
-  NanCallback *callback = GetTagCallback(result->tag);
-  Handle<Value> argv[] = {NanError(ErrorMessage())};
-
-  callback->Call(1, argv);
-
-  DestroyTag(result->tag);
-  grpc_event_finish(result);
-  result = NULL;
+  event_callback.Call(1, argv);
 }
 
 }  // namespace node
