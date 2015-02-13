967965601a60a8bbea999f92806a7988c006602c
Sunriver <seriousman1994217winter@gmail.com>
2015-02-13 14:40:41 -0800

    add a simple server streaming e2e test, which passes

--- src/python/src/_framework/foundation/callable_util.py
+++ src/python/src/_framework/foundation/callable_util.py
@@ -29,47 +29,18 @@
 
 """Utilities for working with callables."""
 
-import abc
-import collections
-import enum
 import functools
 import logging
 
-
-class Outcome(object):
-  """A sum type describing the outcome of some call.
-
-  Attributes:
-    kind: One of Kind.RETURNED or Kind.RAISED respectively indicating that the
-      call returned a value or raised an exception.
-    return_value: The value returned by the call. Must be present if kind is
-      Kind.RETURNED.
-    exception: The exception raised by the call. Must be present if kind is
-      Kind.RAISED.
-  """
-  __metaclass__ = abc.ABCMeta
-
-  @enum.unique
-  class Kind(enum.Enum):
-    """Identifies the general kind of the outcome of some call."""
-
-    RETURNED = object()
-    RAISED = object()
-
-
-class _EasyOutcome(
-    collections.namedtuple(
-        '_EasyOutcome', ['kind', 'return_value', 'exception']),
-    Outcome):
-  """A trivial implementation of Outcome."""
+from _framework.foundation import future
 
 
 def _call_logging_exceptions(behavior, message, *args, **kwargs):
   try:
-    return _EasyOutcome(Outcome.Kind.RETURNED, behavior(*args, **kwargs), None)
+    return future.returned(behavior(*args, **kwargs))
   except Exception as e:  # pylint: disable=broad-except
     logging.exception(message)
-    return _EasyOutcome(Outcome.Kind.RAISED, None, e)
+    return future.raised(e)
 
 
 def with_exceptions_logged(behavior, message):
@@ -101,7 +72,7 @@ def call_logging_exceptions(behavior, message, *args, **kwargs):
     **kwargs: Keyword arguments to pass to the given behavior.
 
   Returns:
-    An Outcome describing whether the given behavior returned a value or raised
-      an exception.
+    A future.Outcome describing whether the given behavior returned a value or
+      raised an exception.
   """
   return _call_logging_exceptions(behavior, message, *args, **kwargs)
