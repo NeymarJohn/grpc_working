7a93c7d801003fd7110ca1d69b1ac49c7f905e51
Sunriver <seriousman1994217winter@gmail.com>
2015-02-17 18:28:23 -0800

    Updates the module name in the idiomatic and stub layers

--- src/objective-c/GRPCClient/private/NSDictionary+GRPC.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-struct grpc_metadata;
-
-@interface NSDictionary (GRPC)
-+ (instancetype)grpc_dictionaryFromMetadata:(struct grpc_metadata *)entries count:(size_t)count;
-@end
--- src/objective-c/RxLibrary/GRXWriter+Transformations.h
+++
@@ -1,9 +0,0 @@
-#import "GRXWriter.h"
-
-@interface GRXWriter (Transformations)
-
-// Returns a writer that wraps the receiver, and has all the values the receiver would write
-// transformed by the provided mapping function.
-- (GRXWriter *)map:(id (^)(id value))map;
-
-@end
