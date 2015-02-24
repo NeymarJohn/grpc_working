7a59b4f1ac469b574430a44f30b54a84b45876f8
Sunriver <seriousman1994217winter@gmail.com>
2015-02-24 03:58:40 -0800

    Fixed Maven version

--- go/greeter_client/main.go
+++ go/greeter_client/main.go
@@ -49,18 +49,19 @@ const (
 
 func main() {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(address)
+	var opts []grpc.DialOption
+	conn, err := grpc.Dial(address, opts...)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
 
 	// Contact the server and print out its response.
 	name := defaultName
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
+	c := pb.NewGreeterClient(conn)
 	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
