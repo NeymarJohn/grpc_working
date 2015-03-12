1a1494cb7e45f5a32e16ee3772083ac23fd1d4de
Sunriver <seriousman1994217winter@gmail.com>
2015-03-12 22:57:22 +0900

    Fix client sending invalid GOAWAY last-stream-id

--- include/grpc++/completion_queue.h
+++ include/grpc++/completion_queue.h
@@ -34,7 +34,6 @@
 #ifndef GRPCXX_COMPLETION_QUEUE_H
 #define GRPCXX_COMPLETION_QUEUE_H
 
-#include <grpc/support/time.h>
 #include <grpc++/impl/client_unary_call.h>
 
 struct grpc_completion_queue;
@@ -76,18 +75,10 @@ class CompletionQueue {
   explicit CompletionQueue(grpc_completion_queue *take);
   ~CompletionQueue();
 
-  // Tri-state return for AsyncNext: SHUTDOWN, GOT_EVENT, TIMEOUT
-  enum NextStatus {SHUTDOWN, GOT_EVENT, TIMEOUT};
-
-  // Nonblocking (until deadline) read from queue.
-  // Cannot rely on result of tag or ok if return is TIMEOUT
-  NextStatus AsyncNext(void **tag, bool *ok, gpr_timespec deadline);
-
-  // Blocking (until deadline) read from queue.
-  // Returns false if the queue is ready for destruction, true if event
-  bool Next(void **tag, bool *ok) {
-    return (AsyncNext(tag,ok,gpr_inf_future) != SHUTDOWN);
-  }
+  // Blocking read from queue.
+  // Returns true if an event was received, false if the queue is ready
+  // for destruction.
+  bool Next(void **tag, bool *ok);
 
   // Shutdown has to be called, and the CompletionQueue can only be
   // destructed when false is returned from Next().
--- src/cpp/common/completion_queue.cc
+++ src/cpp/common/completion_queue.cc
@@ -57,23 +57,19 @@ class EventDeleter {
   }
 };
 
-CompletionQueue::NextStatus CompletionQueue::AsyncNext(void** tag, bool* ok,
-                                                       gpr_timespec deadline) {
+bool CompletionQueue::Next(void** tag, bool* ok) {
   std::unique_ptr<grpc_event, EventDeleter> ev;
 
   for (;;) {
-    ev.reset(grpc_completion_queue_next(cq_, deadline));
-    if (!ev) { /* got a NULL back because deadline passed */
-      return TIMEOUT;
-    }
+    ev.reset(grpc_completion_queue_next(cq_, gpr_inf_future));
     if (ev->type == GRPC_QUEUE_SHUTDOWN) {
-      return SHUTDOWN;
+      return false;
     }
     auto cq_tag = static_cast<CompletionQueueTag*>(ev->tag);
     *ok = ev->data.op_complete == GRPC_OP_OK;
     *tag = cq_tag;
     if (cq_tag->FinalizeResult(tag, ok)) {
-      return GOT_EVENT;
+      return true;
     }
   }
 }
