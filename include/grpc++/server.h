6b8862e0734b24e028266b4df1bdb9ff4d756a6e
Sunriver <seriousman1994217winter@gmail.com>
2015-03-13 16:06:56 -0700

    Add error logs when credentials are created with non-positive lifetimes

--- include/grpc++/server.h
+++ include/grpc++/server.h
@@ -49,8 +49,6 @@ struct grpc_server;
 
 namespace grpc {
 class AsynchronousService;
-class GenericServerContext;
-class AsyncGenericService;
 class RpcService;
 class RpcServiceMethod;
 class ServerCredentials;
@@ -70,10 +68,7 @@ class Server GRPC_FINAL : private CallHook,
   // function to ever return)
   void Wait();
 
-  CompletionQueue* cq() { return &cq_; }
-
  private:
-  friend class AsyncGenericService;
   friend class ServerBuilder;
 
   class SyncRequest;
@@ -86,7 +81,6 @@ class Server GRPC_FINAL : private CallHook,
   // The service must exist for the lifetime of the Server instance.
   bool RegisterService(RpcService* service);
   bool RegisterAsyncService(AsynchronousService* service);
-  void RegisterAsyncGenericService(AsyncGenericService* service);
   // Add a listening port. Can be called multiple times.
   int AddPort(const grpc::string& addr, ServerCredentials* creds);
   // Start the server.
@@ -103,10 +97,6 @@ class Server GRPC_FINAL : private CallHook,
                         grpc::protobuf::Message* request,
                         ServerAsyncStreamingInterface* stream,
                         CompletionQueue* cq, void* tag) GRPC_OVERRIDE;
-
-  void RequestAsyncGenericCall(GenericServerContext* context,
-                               ServerAsyncStreamingInterface* stream,
-                               CompletionQueue* cq, void* tag);
 
   // Completion queue.
   CompletionQueue cq_;
