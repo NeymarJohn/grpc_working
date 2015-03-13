f7c2f5ed39419b55f8dd4a64ed5b57074171e223
Sunriver <seriousman1994217winter@gmail.com>
2015-03-13 18:36:39 +0100

    Moving ZeroCopyOutputStream to grpc++/config.h.

--- src/cpp/client/insecure_credentials.cc
+++ src/cpp/client/insecure_credentials.cc
@@ -54,7 +54,7 @@ class InsecureCredentialsImpl GRPC_FINAL : public Credentials {
         target, grpc_channel_create(target.c_str(), &channel_args)));
   }
 
-  SecureCredentials* AsSecureCredentials() GRPC_OVERRIDE { return nullptr; }
+  SecureCredentials* AsSecureCredentials() { return nullptr; }
 };
 }  // namespace
 
