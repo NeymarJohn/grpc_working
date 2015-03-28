34eb7f5dc785b69cdf120039e3e492db1b6f83ba
Sunriver <seriousman1994217winter@gmail.com>
2015-03-28 15:56:47 -0700

    update the generated code for hello world proto

--- python/route_guide/route_guide_client.py
+++ python/route_guide/route_guide_client.py
@@ -39,9 +39,10 @@ _TIMEOUT_SECONDS = 30
 
 
 def make_route_note(message, latitude, longitude):
-  return route_guide_pb2.RouteNote(
-      message=message,
-      location=route_guide_pb2.Point(latitude=latitude, longitude=longitude))
+  route_note = route_guide_pb2.RouteNote(message=message)
+  route_note.location.latitude = latitude
+  route_note.location.longitude = longitude
+  return route_note
 
 
 def guide_get_one_feature(stub, point):
@@ -62,11 +63,11 @@ def guide_get_feature(stub):
 
 
 def guide_list_features(stub):
-  rect = route_guide_pb2.Rectangle(
-      lo=route_guide_pb2.Point(
-          latitude=400000000, longitude = -750000000),
-      hi=route_guide_pb2.Point(
-          latitude = 420000000, longitude = -730000000))
+  rect = route_guide_pb2.Rectangle()
+  rect.lo.latitude = 400000000
+  rect.lo.longitude = -750000000
+  rect.hi.latitude = 420000000
+  rect.hi.longitude = -730000000
   print "Looking for features between 40, -75 and 42, -73"
 
   features = stub.ListFeatures(rect, _TIMEOUT_SECONDS)
@@ -117,13 +118,13 @@ def guide_route_chat(stub):
 def run():
   with route_guide_pb2.early_adopter_create_RouteGuide_stub('localhost', 50051) as stub:
     print "-------------- GetFeature --------------"
-    guide_get_feature(stub);
+    guide_get_feature(stub)
     print "-------------- ListFeatures --------------"
-    guide_list_features(stub);
+    guide_list_features(stub)
     print "-------------- RecordRoute --------------"
-    guide_record_route(stub);
+    guide_record_route(stub)
     print "-------------- RouteChat --------------"
-    guide_route_chat(stub);
+    guide_route_chat(stub)
 
 
 if __name__ == '__main__':
