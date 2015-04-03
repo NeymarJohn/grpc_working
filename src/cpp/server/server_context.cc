9a0d6d5cef159624d6ede4d6daad567acd866832
Sunriver <seriousman1994217winter@gmail.com>
2015-04-03 14:02:12 +0800

    change gflags in .gitmodules to github

--- src/cpp/server/server_context.cc
+++ src/cpp/server/server_context.cc
@@ -33,8 +33,9 @@
 
 #include <grpc++/server_context.h>
 
+#include <mutex>
+
 #include <grpc++/impl/call.h>
-#include <grpc++/impl/sync.h>
 #include <grpc/grpc.h>
 #include <grpc/support/log.h>
 #include "src/cpp/util/time.h"
@@ -56,14 +57,14 @@ class ServerContext::CompletionOp GRPC_FINAL : public CallOpBuffer {
   void Unref();
 
  private:
-  grpc::mutex mu_;
+  std::mutex mu_;
   int refs_;
   bool finalized_;
   bool cancelled_;
 };
 
 void ServerContext::CompletionOp::Unref() {
-  grpc::unique_lock<grpc::mutex> lock(mu_);
+  std::unique_lock<std::mutex> lock(mu_);
   if (--refs_ == 0) {
     lock.unlock();
     delete this;
@@ -72,13 +73,13 @@ void ServerContext::CompletionOp::Unref() {
 
 bool ServerContext::CompletionOp::CheckCancelled(CompletionQueue* cq) {
   cq->TryPluck(this);
-  grpc::lock_guard<grpc::mutex> g(mu_);
+  std::lock_guard<std::mutex> g(mu_);
   return finalized_ ? cancelled_ : false;
 }
 
 bool ServerContext::CompletionOp::FinalizeResult(void** tag, bool* status) {
   GPR_ASSERT(CallOpBuffer::FinalizeResult(tag, status));
-  grpc::unique_lock<grpc::mutex> lock(mu_);
+  std::unique_lock<std::mutex> lock(mu_);
   finalized_ = true;
   if (!*status) cancelled_ = true;
   if (--refs_ == 0) {
