8baf76329844656031be8f51975abc1926c3e5c6
Sunriver <seriousman1994217winter@gmail.com>
2015-04-17 13:30:51 -0700

    Added test case demonstrating cancel on bidi stream

--- src/ruby/spec/generic/rpc_server_pool_spec.rb
+++ src/ruby/spec/generic/rpc_server_pool_spec.rb
@@ -30,9 +30,9 @@
 require 'grpc'
 require 'xray/thread_dump_signal_handler'
 
-describe GRPC::Pool do
-  Pool = GRPC::Pool
+Pool = GRPC::RpcServer::Pool
 
+describe Pool do
   describe '#new' do
     it 'raises if a non-positive size is used' do
       expect { Pool.new(0) }.to raise_error
