3db78badfc8268762b677ae05a8b75ab210e987c
Sunriver <seriousman1994217winter@gmail.com>
2015-04-23 14:32:33 -0700

    Corrects command/flags used in ruby interop tests

--- src/objective-c/ProtoRPC/ProtoRPC.h
+++ src/objective-c/ProtoRPC/ProtoRPC.h
@@ -31,49 +31,16 @@
  *
  */
 
-#include <grpc/grpc.h>
+#import <Foundation/Foundation.h>
+#import <gRPC/GRPCCall.h>
 
-#include "test/core/end2end/cq_verifier.h"
-#include "test/core/util/test_config.h"
-#include <grpc/support/log.h>
+@interface ProtoRPC : GRPCCall
 
-static void *tag(gpr_intptr x) { return (void *)x; }
+- (instancetype)initWithHost:(NSString *)host
+                      method:(GRPCMethodName *)method
+              requestsWriter:(id<GRXWriter>)requestsWriter
+               responseClass:(Class)responseClass
+          responsesWriteable:(id<GRXWriteable>)responsesWriteable NS_DESIGNATED_INITIALIZER;
 
-int main(int argc, char **argv) {
-  grpc_channel *chan;
-  grpc_call *call;
-  grpc_metadata md = {"a", "b", 1, {{NULL, NULL, NULL}}};
-  grpc_completion_queue *cq;
-  cq_verifier *cqv;
-
-  grpc_test_init(argc, argv);
-  grpc_init();
-
-  chan = grpc_lame_client_channel_create();
-  GPR_ASSERT(chan);
-  call = grpc_channel_create_call_old(chan, "/Foo", "anywhere",
-                                      GRPC_TIMEOUT_SECONDS_TO_DEADLINE(100));
-  GPR_ASSERT(call);
-  cq = grpc_completion_queue_create();
-  cqv = cq_verifier_create(cq);
-
-  /* we should be able to add metadata */
-  GPR_ASSERT(GRPC_CALL_OK == grpc_call_add_metadata_old(call, &md, 0));
-
-  /* and invoke the call */
-  GPR_ASSERT(GRPC_CALL_OK == grpc_call_invoke_old(call, cq, tag(2), tag(3), 0));
-
-  /* the call should immediately fail */
-  cq_expect_client_metadata_read(cqv, tag(2), NULL);
-  cq_expect_finished(cqv, tag(3), NULL);
-  cq_verify(cqv);
-
-  grpc_call_destroy(call);
-  grpc_channel_destroy(chan);
-  cq_verifier_destroy(cqv);
-  grpc_completion_queue_destroy(cq);
-
-  grpc_shutdown();
-
-  return 0;
-}
+- (void)start;
+@end
