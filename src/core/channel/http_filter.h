89c0cab30138bbaaf8b2da4fd8f112dda7e60ced
Sunriver <seriousman1994217winter@gmail.com>
2015-04-24 01:12:18 +0200

    Forgot to rename header guard.

--- src/core/channel/http_filter.h
+++ src/core/channel/http_filter.h
@@ -31,21 +31,13 @@
  *
  */
 
-#ifndef GRPC_INTERNAL_CPP_UTIL_TIME_H
-#define GRPC_INTERNAL_CPP_UTIL_TIME_H
+#ifndef GRPC_INTERNAL_CORE_CHANNEL_HTTP_FILTER_H
+#define GRPC_INTERNAL_CORE_CHANNEL_HTTP_FILTER_H
 
-#include <chrono>
+#include "src/core/channel/channel_stack.h"
 
-#include <grpc/support/time.h>
+/* Processes metadata that is common to both client and server for HTTP2
+   transports. */
+extern const grpc_channel_filter grpc_http_filter;
 
-namespace grpc {
-
-// from and to should be absolute time.
-void Timepoint2Timespec(const std::chrono::system_clock::time_point& from,
-                        gpr_timespec* to);
-
-std::chrono::system_clock::time_point Timespec2Timepoint(gpr_timespec t);
-
-}  // namespace grpc
-
-#endif  // GRPC_INTERNAL_CPP_UTIL_TIME_H
+#endif  /* GRPC_INTERNAL_CORE_CHANNEL_HTTP_FILTER_H */
