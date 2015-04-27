e4623e504b93657f74e6c696508cdac672f9ecfd
Sunriver <seriousman1994217winter@gmail.com>
2015-04-27 12:31:36 -0700

    Simplified an initializer call

--- src/objective-c/GRPCClient/private/GRPCCompletionQueue.h
+++ src/objective-c/GRPCClient/private/GRPCCompletionQueue.h
@@ -32,11 +32,11 @@
  */
 
 #import <Foundation/Foundation.h>
+#include <grpc/grpc.h>
 
 struct grpc_completion_queue;
-struct grpc_event;
 
-typedef void(^GRPCEventHandler)(struct grpc_event *event);
+typedef void(^GRPCQueueCompletionHandler)(grpc_op_error error);
 
 // This class lets one more easily use grpc_completion_queue. To use it, pass
 // the value of the unmanagedQueue property of an instance of this class to
