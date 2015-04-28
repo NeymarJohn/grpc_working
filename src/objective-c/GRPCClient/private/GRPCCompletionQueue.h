c40c5ff654a34714ece8d4b80ccd57e1fdf5cce9
Sunriver <seriousman1994217winter@gmail.com>
2015-04-28 09:51:18 -0700

    fixed grpc_docker.sh to run C# auth tests correctly

--- src/objective-c/GRPCClient/private/GRPCCompletionQueue.h
+++ src/objective-c/GRPCClient/private/GRPCCompletionQueue.h
@@ -32,11 +32,11 @@
  */
 
 #import <Foundation/Foundation.h>
-#include <grpc/grpc.h>
 
 struct grpc_completion_queue;
+struct grpc_event;
 
-typedef void(^GRPCQueueCompletionHandler)(grpc_op_error error);
+typedef void(^GRPCEventHandler)(struct grpc_event *event);
 
 // This class lets one more easily use grpc_completion_queue. To use it, pass
 // the value of the unmanagedQueue property of an instance of this class to
