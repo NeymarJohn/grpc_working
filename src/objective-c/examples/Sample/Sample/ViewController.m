6507757e5bb79d8a56dbd1b9c089a1508228b56f
Sunriver <seriousman1994217winter@gmail.com>
2015-04-27 23:59:22 -0700

    Fixes build by moving the prepare_command back up to the top level

---
+++ src/objective-c/examples/Sample/Sample/ViewController.m
@@ -0,0 +1,75 @@
+/*
+ *
+ * Copyright 2015, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#import "ViewController.h"
+
+#import <gRPC/GRPCCall.h>
+#import <gRPC/GRPCMethodName.h>
+#import <gRPC/GRXWriter+Immediate.h>
+#import <gRPC/GRXWriteable.h>
+#import <RemoteTest/Messages.pb.h>
+
+@interface ViewController ()
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+
+  GRPCMethodName *method = [[GRPCMethodName alloc] initWithPackage:@"grpc.testing"
+                                                         interface:@"TestService"
+                                                            method:@"UnaryCall"];
+
+  RMTSimpleRequest *request = [[[[[[RMTSimpleRequestBuilder alloc] init]
+                                  setResponseSize:100]
+                                 setFillUsername:YES]
+                                setFillOauthScope:YES]
+                               build];
+  id<GRXWriter> requestsWriter = [GRXWriter writerWithValue:[request data]];
+
+  GRPCCall *call = [[GRPCCall alloc] initWithHost:@"grpc-test.sandbox.google.com"
+                                           method:method
+                                   requestsWriter:requestsWriter];
+
+  id<GRXWriteable> responsesWriteable = [[GRXWriteable alloc] initWithValueHandler:^(NSData *value) {
+    RMTSimpleResponse *response = [RMTSimpleResponse parseFromData:value];
+    NSLog(@"Received response:\n%@", response);
+  } completionHandler:^(NSError *errorOrNil) {
+    NSLog(@"Finished with error: %@", errorOrNil);
+  }];
+
+  [call startWithWriteable:responsesWriteable];
+}
+
+@end
---
+++ src/python/README.md
@@ -0,0 +1,75 @@
+gRPC Python
+=========
+
+The Python facility of gRPC.
+
+
+Status
+-------
+
+Usable with limitations, Pre-Alpha
+
+Prerequisites
+-----------------------
+
+Python 2.7, virtualenv, pip, libprotobuf-dev, and libprotoc-dev.
+
+
+Building from source
+----------------------
+
+- Build the gRPC core from the root of the
+  [gRPC git repo](https://github.com/grpc/grpc)
+```
+$ make shared_c static_c
+```
+
+- Use build_python.sh to build the Python code and install it into a virtual environment
+```
+$ tools/run_tests/build_python.sh
+```
+
+
+Testing
+-----------------------
+
+- Use run_python.sh to run gRPC as it was installed into the virtual environment
+```
+$ tools/run_tests/run_python.sh
+```
+
+
+Installing
+-----------------------
+
+- Install the gRPC core
+  - [Debian package](https://github.com/grpc/grpc/releases)
+    ```
+    $ wget https://github.com/grpc/grpc/releases/download/release-0_5_0/libgrpc_0.5.0_amd64.deb
+    $ wget https://github.com/grpc/grpc/releases/download/release-0_5_0/libgrpc-dev_0.5.0_amd64.deb
+    $ sudo dpkg -i libgrpc_0.5.0_amd64.deb libgrpc-dev_0.5.0_amd64.deb
+    ```
+  - [From source](https://github.com/grpc/grpc/blob/master/INSTALL)
+
+- Install gRPC Python's dependencies
+```
+$ pip install -r src/python/requirements.txt
+```
+
+- Install gRPC Python
+```
+$ pip install src/python/src
+```
+
+Packaging to PyPI
+-----------------------
+
+- Install packaging dependencies
+```
+$ pip install setuptools twine
+```
+
+- Push to PyPI
+```
+$ ../../tools/distrib/python/submit.py
+```
