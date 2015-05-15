735a497eaed4721db507fbdacf8808d2a0072b85
Sunriver <seriousman1994217winter@gmail.com>
2015-05-15 14:08:48 -0700

    update code to reflect API changes

--- tools/dockerfile/grpc_java/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -rf /var/local/git
-cp -R /var/local/git-clone /var/local/git
-cd /var/local/git/grpc-java/lib/netty && \
-  mvn -pl codec-http2 -am -DskipTests install clean
-cd /var/local/git/grpc-java && \
-  ./gradlew build installDist
-
-echo 'build finished'
