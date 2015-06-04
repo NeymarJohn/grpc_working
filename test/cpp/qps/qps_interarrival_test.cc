70d7e704950c71c63b724ae8746b923890651dbf
Sunriver <seriousman1994217winter@gmail.com>
2015-06-04 10:48:43 -0700

    Minor fixes

--- test/cpp/qps/qps_interarrival_test.cc
+++ test/cpp/qps/qps_interarrival_test.cc
@@ -31,13 +31,47 @@
  *
  */
 
-#ifndef GRPC_INTERNAL_CORE_DEBUG_TRACE_H
-#define GRPC_INTERNAL_CORE_DEBUG_TRACE_H
+#include "test/cpp/qps/interarrival.h"
+#include <chrono>
+#include <iostream>
 
-#include <grpc/support/port_platform.h>
+// Use the C histogram rather than C++ to avoid depending on proto
+#include <grpc/support/histogram.h>
+#include <grpc++/config.h>
 
-void grpc_register_tracer(const char *name, int *flag);
-void grpc_tracer_init(const char *env_var_name);
-void grpc_tracer_shutdown(void);
+using grpc::testing::ExpDist;
+using grpc::testing::InterarrivalTimer;
 
-#endif  /* GRPC_INTERNAL_CORE_DEBUG_TRACE_H */
+void RunTest(InterarrivalTimer&& timer, std::string title) {
+  gpr_histogram *h(gpr_histogram_create(0.01,60e9));
+  
+  for (int i=0; i<10000000; i++) {
+    for (int j=0; j<5; j++) {
+      gpr_histogram_add(h, timer(j).count());
+    }
+  }
+  
+  std::cout << title <<  " Distribution" << std::endl;
+  std::cout << "Value, Percentile" << std::endl;
+  for (double pct = 0.0; pct < 100.0; pct += 1.0) {
+    std::cout << gpr_histogram_percentile(h, pct) << "," << pct << std::endl;
+  }
+  
+  gpr_histogram_destroy(h);
+}
+
+using grpc::testing::ExpDist;
+using grpc::testing::DetDist;
+using grpc::testing::UniformDist;
+using grpc::testing::ParetoDist;
+
+int main(int argc, char **argv) {
+  RunTest(InterarrivalTimer(ExpDist(10.0), 5), std::string("Exponential(10)"));
+  RunTest(InterarrivalTimer(DetDist(5.0), 5), std::string("Det(5)"));
+  RunTest(InterarrivalTimer(UniformDist(0.0,10.0), 5),
+          std::string("Uniform(1,10)"));
+  RunTest(InterarrivalTimer(ParetoDist(1.0,1.0), 5),
+          std::string("Pareto(1,1)"));
+
+  return 0;
+}
