6dadf22ad1ce2f071aee38cdadc0032c37962257
Sunriver <seriousman1994217winter@gmail.com>
2015-06-16 22:41:18 -0700

    First request/response with separated chttp2 lock passes

--- src/python/src/grpc/_adapter/_c/utility.c
+++ src/python/src/grpc/_adapter/_c/utility.c
@@ -41,6 +41,7 @@
 #include <grpc/support/slice.h>
 #include <grpc/support/time.h>
 #include <grpc/support/string_util.h>
+#include <grpc/support/log.h>
 
 #include "grpc/_adapter/_c/types.h"
 
@@ -123,7 +124,8 @@ PyObject *pygrpc_consume_event(grpc_event event) {
           event.success ? Py_True : Py_False);
     } else {
       result = Py_BuildValue("iOOONO", GRPC_OP_COMPLETE, tag->user_tag,
-          tag->call, Py_None, pygrpc_consume_ops(tag->ops, tag->nops),
+          tag->call ? tag->call : Py_None, Py_None,
+          pygrpc_consume_ops(tag->ops, tag->nops),
           event.success ? Py_True : Py_False);
     }
     break;
@@ -168,7 +170,6 @@ int pygrpc_produce_op(PyObject *op, grpc_op *result) {
     return 0;
   }
   c_op.op = type;
-  c_op.flags = 0;
   switch (type) {
   case GRPC_OP_SEND_INITIAL_METADATA:
     if (!pygrpc_cast_pylist_to_send_metadata(
@@ -196,11 +197,10 @@ int pygrpc_produce_op(PyObject *op, grpc_op *result) {
       return 0;
     }
     if (!PyTuple_Check(PyTuple_GET_ITEM(op, STATUS_INDEX))) {
-      char *buf;
-      gpr_asprintf(&buf, "expected tuple status in op of length %d",
-                   STATUS_TUPLE_SIZE);
-      PyErr_SetString(PyExc_ValueError, buf);
-      gpr_free(buf);
+      char buf[64];
+      snprintf(buf, sizeof(buf), "expected tuple status in op of length %d",
+               STATUS_TUPLE_SIZE);
+      PyErr_SetString(PyExc_TypeError, buf);
       return 0;
     }
     c_op.data.send_status_from_server.status = PyInt_AsLong(
--- test/cpp/util/cli_call_test.cc
+++ test/cpp/util/cli_call_test.cc
@@ -108,7 +108,7 @@ TEST_F(CliCallTest, SimpleRpc) {
   ClientContext context;
   Status s = stub_->Echo(&context, request, &response);
   EXPECT_EQ(response.message(), request.message());
-  EXPECT_TRUE(s.ok());
+  EXPECT_TRUE(s.IsOk());
 
   const grpc::string kMethod("/grpc.cpp.test.util.TestService/Echo");
   grpc::string request_bin, response_bin, expected_response_bin;
