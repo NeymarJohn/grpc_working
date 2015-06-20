8a77685e647c9f22cb2649f08ffcc602b35c0868
Sunriver <seriousman1994217winter@gmail.com>
2015-06-19 17:27:53 -0700

    Always use the first status code received

--- test/cpp/qps/report.h
+++ test/cpp/qps/report.h
@@ -41,7 +41,6 @@
 
 #include "test/cpp/qps/driver.h"
 #include "test/cpp/qps/qpstest.grpc.pb.h"
-#include "perf_db_client.h"
 
 namespace grpc {
 namespace testing {
@@ -102,27 +101,6 @@ class GprLogReporter : public Reporter {
   void ReportQPSPerCore(const ScenarioResult& result) const GRPC_OVERRIDE;
   void ReportLatency(const ScenarioResult& result) const GRPC_OVERRIDE;
   void ReportTimes(const ScenarioResult& result) const GRPC_OVERRIDE;
-};
-
-/** Reporter for client leaderboard. */
-class PerfDbReporter : public Reporter {
- public:
-  PerfDbReporter(const string& name, const string& access_token, const string& test_name, const string& sys_info, const string& server_address)
-   : Reporter(name), access_token_(access_token), test_name_(test_name), sys_info_(sys_info) {
-    perfDbClient.init(grpc::CreateChannel(server_address, grpc::InsecureCredentials(), ChannelArguments()));
-  }
-  ~PerfDbReporter() { SendData(); };
-
- private:
-  PerfDbClient perfDbClient;
-  std::string access_token_;
-  std::string test_name_;
-  std::string sys_info_;
-  void ReportQPS(const ScenarioResult& result) const GRPC_OVERRIDE;
-  void ReportQPSPerCore(const ScenarioResult& result) const GRPC_OVERRIDE;
-  void ReportLatency(const ScenarioResult& result) const GRPC_OVERRIDE;
-  void ReportTimes(const ScenarioResult& result) const GRPC_OVERRIDE;
-  void SendData() const;
 };
 
 }  // namespace testing
