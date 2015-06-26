0ad1d9b3fa177115225e9fbfc397e1199f849a52
Sunriver <seriousman1994217winter@gmail.com>
2015-06-26 09:02:20 -0700

    client_config: chttp2_fullstack_no_op_unsecure_test passes

--- src/core/client_config/subchannel_factory.c
+++ src/core/client_config/subchannel_factory.c
@@ -31,30 +31,4 @@
  *
  */
 
-#include <grpc/grpc.h>
-#include "test/core/util/test_config.h"
-
-int main(int argc, char **argv) {
-  grpc_completion_queue *cq1;
-  grpc_completion_queue *cq2;
-  grpc_server *server;
-
-  grpc_test_init(argc, argv);
-  grpc_init();
-  cq1 = grpc_completion_queue_create();
-  cq2 = grpc_completion_queue_create();
-  server = grpc_server_create(NULL);
-  grpc_server_register_completion_queue(server, cq1);
-  grpc_server_add_http2_port(server, "[::]:0");
-  grpc_server_register_completion_queue(server, cq2);
-  grpc_server_start(server);
-  grpc_server_shutdown_and_notify(server, cq2, NULL);
-  grpc_completion_queue_next(cq2, gpr_inf_future);  /* cue queue hang */
-  grpc_completion_queue_shutdown(cq1);
-  grpc_completion_queue_shutdown(cq2);
-  grpc_completion_queue_next(cq1, gpr_inf_future);
-  grpc_completion_queue_next(cq2, gpr_inf_future);
-  grpc_server_destroy(server);
-  grpc_shutdown();
-  return 0;
-}
+#include "src/core/client_config/subchannel_factory.h"
