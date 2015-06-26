d6b3a493708d6d782c89bf804835db8b58c82034
Sunriver <seriousman1994217winter@gmail.com>
2015-06-25 18:02:18 -0700

    BLARGHAFLARGHAAHAJHAKJHATJHKAYMNHZIYRN

--- src/core/surface/client.h
+++ src/core/surface/client.h
@@ -31,26 +31,11 @@
  *
  */
 
-#ifndef GRPC_INTERNAL_CORE_CLIENT_CONFIG_CONNECTOR_H
-#define GRPC_INTERNAL_CORE_CLIENT_CONFIG_CONNECTOR_H
+#ifndef GRPC_INTERNAL_CORE_SURFACE_CLIENT_H
+#define GRPC_INTERNAL_CORE_SURFACE_CLIENT_H
 
-#include "src/core/transport/transport.h"
+#include "src/core/channel/channel_stack.h"
 
-typedef struct grpc_connector grpc_connector;
-typedef struct grpc_connector_vtable grpc_connector_vtable;
+extern const grpc_channel_filter grpc_client_surface_filter;
 
-struct grpc_connector {
-  const grpc_connector_vtable *vtable;
-};
-
-struct grpc_connector_vtable {
-  void (*ref)(grpc_connector *connector);
-  void (*unref)(grpc_connector *connector);
-  void (*connect)(grpc_connector *connector, const grpc_channel_args *channel_args, grpc_mdctx *metadata_context, grpc_transport **transport, grpc_iomgr_closure *notify);
-};
-
-void grpc_connector_ref(grpc_connector *connector);
-void grpc_connector_unref(grpc_connector *connector);
-void grpc_connector_connect(grpc_connector *connector, const grpc_channel_args *channel_args, grpc_mdctx *metadata_context, grpc_transport **transport, grpc_iomgr_closure *notify);
-
-#endif
+#endif  /* GRPC_INTERNAL_CORE_SURFACE_CLIENT_H */
