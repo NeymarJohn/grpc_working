e27d22018b6d092b1af9fb553263836e3056e749
Sunriver <seriousman1994217winter@gmail.com>
2015-07-01 14:46:42 -0700

    Fix a TSAN reported race

--- src/core/httpcli/httpcli.c
+++ src/core/httpcli/httpcli.c
@@ -198,7 +198,7 @@ static void on_connected(void *arg, grpc_endpoint *tcp) {
                GRPC_SECURITY_OK);
     grpc_setup_secure_transport(&sc->base, tcp, on_secure_transport_setup_done,
                                 req);
-    GRPC_SECURITY_CONNECTOR_UNREF(&sc->base, "httpcli");
+    grpc_security_connector_unref(&sc->base);
   } else {
     start_write(req);
   }
