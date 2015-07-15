97ea7640568c56030a9df2aff5663f568ba5bac5
Sunriver <seriousman1994217winter@gmail.com>
2015-07-15 13:36:27 -0700

    update according to spec change

--- include/grpc/grpc.h
+++ include/grpc/grpc.h
@@ -351,9 +351,6 @@ void grpc_init(void);
     destroyed. */
 void grpc_shutdown(void);
 
-/** Return a string representing the current version of grpc */
-const char *grpc_version_string(void);
-
 /** Create a completion queue */
 grpc_completion_queue *grpc_completion_queue_create(void);
 
