9bea65582b236897c763e8c63fc48e33225bcf6a
Sunriver <seriousman1994217winter@gmail.com>
2015-07-17 08:48:56 -0700

    rename script to run_distribution

--- src/core/security/security_context.h
+++ src/core/security/security_context.h
@@ -34,13 +34,11 @@
 #ifndef GRPC_INTERNAL_CORE_SECURITY_SECURITY_CONTEXT_H
 #define GRPC_INTERNAL_CORE_SECURITY_SECURITY_CONTEXT_H
 
-#include "src/core/iomgr/pollset.h"
 #include "src/core/security/credentials.h"
 
-/* --- grpc_auth_ticket --- */
-struct grpc_auth_ticket {
-  grpc_pollset *pollset;
-};
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /* --- grpc_auth_context ---
 
@@ -48,18 +46,17 @@ struct grpc_auth_ticket {
 
 /* Property names are always NULL terminated. */
 
-typedef struct {
-  grpc_auth_property *array;
-  size_t count;
-  size_t capacity;
-} grpc_auth_property_array;
-
 struct grpc_auth_context {
   struct grpc_auth_context *chained;
-  grpc_auth_property_array properties;
+  grpc_auth_property *properties;
+  size_t property_count;
   gpr_refcount refcount;
   const char *peer_identity_property_name;
 };
+
+/* Constructor. */
+grpc_auth_context *grpc_auth_context_create(grpc_auth_context *chained,
+                                            size_t property_count);
 
 /* Refcounting. */
 #ifdef GRPC_AUTH_CONTEXT_REFCOUNT_DEBUG
@@ -78,6 +75,12 @@ void grpc_auth_context_unref(grpc_auth_context *policy, const char *file,
 grpc_auth_context *grpc_auth_context_ref(grpc_auth_context *policy);
 void grpc_auth_context_unref(grpc_auth_context *policy);
 #endif
+
+grpc_auth_property grpc_auth_property_init_from_cstring(const char *name,
+                                                        const char *value);
+
+grpc_auth_property grpc_auth_property_init(const char *name, const char *value,
+                                           size_t value_length);
 
 void grpc_auth_property_reset(grpc_auth_property *property);
 
@@ -104,10 +107,9 @@ typedef struct {
 grpc_server_security_context *grpc_server_security_context_create(void);
 void grpc_server_security_context_destroy(void *ctx);
 
-/* --- Auth metadata processing. --- */
-
-grpc_process_auth_metadata_func
-grpc_server_auth_context_get_process_metadata_func(void);
+#ifdef __cplusplus
+}
+#endif
 
 #endif  /* GRPC_INTERNAL_CORE_SECURITY_SECURITY_CONTEXT_H */
 
