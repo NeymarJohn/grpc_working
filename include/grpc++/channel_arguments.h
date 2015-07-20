42820857a319f9d99faf13aa30bae3429ee813f9
Sunriver <seriousman1994217winter@gmail.com>
2015-07-19 22:50:18 -0700

    Fix Windows CV

--- include/grpc++/channel_arguments.h
+++ include/grpc++/channel_arguments.h
@@ -57,10 +57,11 @@ class ChannelArguments {
   // grpc specific channel argument setters
   // Set target name override for SSL host name checking.
   void SetSslTargetNameOverride(const grpc::string& name);
-  // TODO(yangg) add flow control optionsc
+  // TODO(yangg) add flow control options
 
   // Set the compression algorithm for the channel.
-  void SetCompressionAlgorithm(grpc_compression_algorithm algorithm);
+  void _Experimental_SetCompressionAlgorithm(
+      grpc_compression_algorithm algorithm);
 
   // Generic channel argument setters. Only for advanced use cases.
   void SetInt(const grpc::string& key, int value);
