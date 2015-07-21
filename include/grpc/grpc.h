10c7a57fffc2c19f316548a87f92882914c75320
Sunriver <seriousman1994217winter@gmail.com>
2015-07-21 16:32:29 -0700

    Rename grpc_channel_create to grpc_insecure_channel_create

--- include/grpc/grpc.h
+++ include/grpc/grpc.h
@@ -126,6 +126,12 @@ typedef struct {
 /** Initial sequence number for http2 transports */
 #define GRPC_ARG_HTTP2_INITIAL_SEQUENCE_NUMBER \
   "grpc.http2.initial_sequence_number"
+/** Primary user agent: goes at the start of the user-agent metadata
+    sent on each request */
+#define GRPC_ARG_PRIMARY_USER_AGENT_STRING "grpc.primary_user_agent"
+/** Secondary user agent: goes at the end of the user-agent metadata
+    sent on each request */
+#define GRPC_ARG_SECONDARY_USER_AGENT_STRING "grpc.secondary_user_agent"
 
 /** Connectivity state of a channel. */
 typedef enum {
@@ -339,12 +345,6 @@ typedef struct grpc_op {
   } data;
 } grpc_op;
 
-/** Registers a plugin to be initialized and deinitialized with the library.
-
-    It is safe to pass NULL to either argument. The initialization and
-    deinitialization order isn't guaranteed. */
-void grpc_register_plugin(void (*init)(void), void (*deinit)(void));
-
 /** Initialize the grpc library.
 
     It is not safe to call any other grpc functions before calling this.
@@ -438,8 +438,8 @@ grpc_call_error grpc_call_start_batch(grpc_call *call, const grpc_op *ops,
     clients will want to simply pass NULL. See grpc_channel_args definition for
     more on this. The data in 'args' need only live through the invocation of
     this function. */
-grpc_channel *grpc_channel_create(const char *target,
-                                  const grpc_channel_args *args);
+grpc_channel *grpc_insecure_channel_create(const char *target,
+                                           const grpc_channel_args *args);
 
 /** Create a lame client: this client fails every operation attempted on it. */
 grpc_channel *grpc_lame_client_channel_create(void);
