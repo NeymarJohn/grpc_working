b55ea88d60256388ca1e999d850a9a34ccc74f8d
Sunriver <seriousman1994217winter@gmail.com>
2015-08-04 11:43:58 -0700

    Update zookeeper in docker again

--- test/build/zookeeper.c
+++ test/build/zookeeper.c
@@ -31,36 +31,13 @@
  *
  */
 
-#ifndef GRPC_INTERNAL_CORE_IOMGR_IOMGR_INTERNAL_H
-#define GRPC_INTERNAL_CORE_IOMGR_IOMGR_INTERNAL_H
+/* This is just a compilation test, to see if we have Zookeeper C client
+   library installed. */
 
-#include "src/core/iomgr/iomgr.h"
-#include <grpc/support/sync.h>
+#include <stdlib.h>
+#include <zookeeper/zookeeper.h>
 
-typedef struct grpc_iomgr_object {
-  char *name;
-  struct grpc_iomgr_object *next;
-  struct grpc_iomgr_object *prev;
-} grpc_iomgr_object;
-
-int grpc_maybe_call_delayed_callbacks(gpr_mu *drop_mu, int success);
-void grpc_iomgr_add_delayed_callback(grpc_iomgr_closure *iocb, int success);
-
-void grpc_iomgr_register_object_internal(grpc_iomgr_object *obj,
-                                         const char *name,
-                                         const char *file,
-                                         int line);
-void grpc_iomgr_unregister_object_internal(grpc_iomgr_object *obj,
-                                           const char *file,
-                                           int line);
-
-#define grpc_iomgr_register_object(obj, name) \
-  grpc_iomgr_register_object_internal(obj, name, __FILE__, __LINE__)
-
-#define grpc_iomgr_unregister_object(obj) \
-  grpc_iomgr_unregister_object_internal(obj, __FILE__, __LINE__)
-
-void grpc_iomgr_platform_init(void);
-void grpc_iomgr_platform_shutdown(void);
-
-#endif  /* GRPC_INTERNAL_CORE_IOMGR_IOMGR_INTERNAL_H */
+int main() {
+  zookeeper_init(NULL, NULL, 0, 0, 0, 0);
+  return 0;
+}
