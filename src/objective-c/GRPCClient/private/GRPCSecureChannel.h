78b1c5580e55fc4b29e05ecd8a2df2c2c82a2121
Sunriver <seriousman1994217winter@gmail.com>
2015-08-05 20:11:02 -0700

    Fixing API (thanks Craig for the comments) and associated tests.

--- src/objective-c/GRPCClient/private/GRPCSecureChannel.h
+++ src/objective-c/GRPCClient/private/GRPCSecureChannel.h
@@ -31,22 +31,8 @@
  *
  */
 
-#include <grpc/grpc.h>
-
 #import "GRPCChannel.h"
 
-struct grpc_credentials;
-
 @interface GRPCSecureChannel : GRPCChannel
-- (instancetype)initWithHost:(NSString *)host;
 
-// Only in tests shouldn't pathToCertificates or hostNameOverride be nil. Passing nil for
-// pathToCertificates results in using the default root certificates distributed with the library.
-- (instancetype)initWithHost:(NSString *)host
-          pathToCertificates:(NSString *)path
-            hostNameOverride:(NSString *)hostNameOverride;
-
-- (instancetype)initWithHost:(NSString *)host
-                 credentials:(struct grpc_credentials *)credentials
-                        args:(grpc_channel_args *)args NS_DESIGNATED_INITIALIZER;
 @end
