18ad6818b13fabb93cb9a8bb811f1ad8562b86ac
Sunriver <seriousman1994217winter@gmail.com>
2015-08-11 17:30:05 -0700

    Fixed lint errors

--- src/ruby/ext/grpc/rb_channel.c
+++ src/ruby/ext/grpc/rb_channel.c
@@ -37,7 +37,6 @@
 
 #include <grpc/grpc.h>
 #include <grpc/grpc_security.h>
-#include <grpc/support/alloc.h>
 #include "rb_grpc.h"
 #include "rb_call.h"
 #include "rb_channel_args.h"
@@ -195,28 +194,15 @@ static VALUE grpc_rb_channel_init_copy(VALUE copy, VALUE orig) {
 
 /* Create a call given a grpc_channel, in order to call method. The request
    is not sent until grpc_call_invoke is called. */
-static VALUE grpc_rb_channel_create_call(VALUE self, VALUE cqueue,
-                                         VALUE parent, VALUE mask,
-                                         VALUE method, VALUE host,
-                                         VALUE deadline) {
+static VALUE grpc_rb_channel_create_call(VALUE self, VALUE cqueue, VALUE method,
+                                         VALUE host, VALUE deadline) {
   VALUE res = Qnil;
   grpc_rb_channel *wrapper = NULL;
   grpc_call *call = NULL;
-  grpc_call *parent_call = NULL;
   grpc_channel *ch = NULL;
   grpc_completion_queue *cq = NULL;
-  int flags = GRPC_PROPAGATE_DEFAULTS;
   char *method_chars = StringValueCStr(method);
-  char *host_chars = NULL;
-  if (host != Qnil) {
-    host_chars = StringValueCStr(host);
-  }
-  if (mask != Qnil) {
-    flags = NUM2UINT(mask);
-  }
-  if (parent != Qnil) {
-    parent_call = grpc_rb_get_wrapped_call(parent);
-  }
+  char *host_chars = StringValueCStr(host);
 
   cq = grpc_rb_get_wrapped_completion_queue(cqueue);
   TypedData_Get_Struct(self, grpc_rb_channel, &grpc_channel_data_type, wrapper);
@@ -226,10 +212,10 @@ static VALUE grpc_rb_channel_create_call(VALUE self, VALUE cqueue,
     return Qnil;
   }
 
-  call = grpc_channel_create_call(ch, parent_call, flags, cq, method_chars,
-                                  host_chars, grpc_rb_time_timeval(
-                                      deadline,
-                                      /* absolute time */ 0));
+  call = grpc_channel_create_call(ch, NULL, GRPC_PROPAGATE_DEFAULTS, cq,
+                                  method_chars, host_chars,
+                                  grpc_rb_time_timeval(deadline,
+                                                       /* absolute time */ 0));
   if (call == NULL) {
     rb_raise(rb_eRuntimeError, "cannot create call with method %s",
              method_chars);
@@ -247,7 +233,6 @@ static VALUE grpc_rb_channel_create_call(VALUE self, VALUE cqueue,
   return res;
 }
 
-
 /* Closes the channel, calling it's destroy method */
 static VALUE grpc_rb_channel_destroy(VALUE self) {
   grpc_rb_channel *wrapper = NULL;
@@ -262,21 +247,6 @@ static VALUE grpc_rb_channel_destroy(VALUE self) {
   }
 
   return Qnil;
-}
-
-
-/* Called to obtain the target that this channel accesses. */
-static VALUE grpc_rb_channel_get_target(VALUE self) {
-  grpc_rb_channel *wrapper = NULL;
-  VALUE res = Qnil;
-  char* target = NULL;
-
-  TypedData_Get_Struct(self, grpc_rb_channel, &grpc_channel_data_type, wrapper);
-  target = grpc_channel_get_target(wrapper->wrapped);
-  res = rb_str_new2(target);
-  gpr_free(target);
-
-  return res;
 }
 
 void Init_grpc_channel() {
@@ -294,8 +264,7 @@ void Init_grpc_channel() {
 
   /* Add ruby analogues of the Channel methods. */
   rb_define_method(grpc_rb_cChannel, "create_call",
-                   grpc_rb_channel_create_call, 6);
-  rb_define_method(grpc_rb_cChannel, "target", grpc_rb_channel_get_target, 0);
+                   grpc_rb_channel_create_call, 4);
   rb_define_method(grpc_rb_cChannel, "destroy", grpc_rb_channel_destroy, 0);
   rb_define_alias(grpc_rb_cChannel, "close", "destroy");
 
