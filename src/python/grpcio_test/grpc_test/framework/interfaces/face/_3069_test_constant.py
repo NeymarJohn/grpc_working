8583b59fed0db42758c11b65f73f0a35036d1210
Sunriver <seriousman1994217winter@gmail.com>
2015-08-26 22:04:30 -0700

    minor fixes

--- src/python/grpcio_test/grpc_test/framework/interfaces/face/_3069_test_constant.py
+++ src/python/grpcio_test/grpc_test/framework/interfaces/face/_3069_test_constant.py
@@ -26,3 +26,12 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""A test constant working around issue 3069."""
+
+# test_constants is referenced from specification in this module.
+from grpc_test.framework.common import test_constants  # pylint: disable=unused-import
+
+# TODO(issue 3069): Replace uses of this constant with
+# test_constants.SHORT_TIMEOUT.
+REALLY_SHORT_TIMEOUT = 0.1
