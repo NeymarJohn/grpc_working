a9b89d3c59533ce166229657bd65244b5a34f685
Sunriver <seriousman1994217winter@gmail.com>
2015-09-01 12:27:15 -0700

    refcounting completion queue shutdown

--- tools/codegen/core/gen_server_registered_method_bad_client_test_body.py
+++ tools/codegen/core/gen_server_registered_method_bad_client_test_body.py
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env python2.7
+
 # Copyright 2015, Google Inc.
 # All rights reserved.
 #
@@ -28,15 +29,48 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-set -ex
+def esc_c(line):
+  out = "\""
+  last_was_hex = False
+  for c in line:
+    if 32 <= c < 127:
+      if c in hex_bytes and last_was_hex:
+        out += "\"\""
+      if c != ord('"'):
+        out += chr(c)
+      else:
+        out += "\\\""
+      last_was_hex = False
+    else:
+      out += "\\x%02x" % c
+      last_was_hex = True
+  return out + "\""
 
-if [ "$CONFIG" = "dbg" ]
-then
-  MSBUILD_CONFIG="Debug"
-else
-  MSBUILD_CONFIG="Release"
-fi
+done = set()
 
-cd $(dirname $0)/../../src/csharp
-
-xbuild /p:Configuration=$MSBUILD_CONFIG Grpc.sln
+for message_length in range(0, 3):
+  for send_message_length in range(0, message_length + 1):
+    payload = [
+      0,
+      (message_length >> 24) & 0xff,
+      (message_length >> 16) & 0xff,
+      (message_length >> 8) & 0xff,
+      (message_length) & 0xff
+    ] + send_message_length * [0]
+    for frame_length in range(0, len(payload) + 1):
+      is_end = frame_length == len(payload) and send_message_length == message_length
+      frame = [
+        (frame_length >> 16) & 0xff,
+        (frame_length >> 8) & 0xff,
+        (frame_length) & 0xff,
+        0,
+        1 if is_end else 0,
+        0, 0, 0, 1
+      ] + payload[0:frame_length]
+      text = esc_c(frame)
+      if text not in done:
+        print 'GRPC_RUN_BAD_CLIENT_TEST(verifier_%s, PFX_STR %s, %s);' % (
+            'succeeds' if is_end else 'fails', 
+            text, 
+            '0' if is_end else 'GRPC_BAD_CLIENT_DISCONNECT')
+        done.add(text)
