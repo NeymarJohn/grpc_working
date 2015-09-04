808aba245ac55765eec8c19600f9844e46a5022f
Sunriver <seriousman1994217winter@gmail.com>
2015-09-03 17:47:14 -0700

    update the interop tests based on spec

--- doc/ref/c++/html/search/classes_4.js
+++ doc/ref/c++/html/search/classes_4.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['genericservercontext',['GenericServerContext',['../classgrpc_1_1_generic_server_context.html',1,'grpc']]],
-  ['genericstub',['GenericStub',['../classgrpc_1_1_generic_stub.html',1,'grpc']]],
-  ['grpclibrary',['GrpcLibrary',['../classgrpc_1_1_grpc_library.html',1,'grpc']]]
+  ['fixedsizethreadpool',['FixedSizeThreadPool',['../classgrpc_1_1_fixed_size_thread_pool.html',1,'grpc']]]
 ];
