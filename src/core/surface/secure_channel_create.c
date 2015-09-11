585beda29441c1d21c8f0dd485f2c88d39950894
Sunriver <seriousman1994217winter@gmail.com>
2015-09-10 22:37:25 -0700

    Fix mac build

--- src/core/surface/secure_channel_create.c
+++ src/core/surface/secure_channel_create.c
@@ -197,7 +197,7 @@ grpc_channel *grpc_secure_channel_create(grpc_credentials *creds,
   subchannel_factory *f;
 #define MAX_FILTERS 3
   const grpc_channel_filter *filters[MAX_FILTERS];
-  size_t n = 0;
+  int n = 0;
 
   GPR_ASSERT(reserved == NULL);
   if (grpc_find_security_connector_in_args(args) != NULL) {
