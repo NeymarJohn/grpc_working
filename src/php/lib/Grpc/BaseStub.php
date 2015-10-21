ebaf39d4a781b4a076eda499e8b39fd8015402cf
Sunriver <seriousman1994217winter@gmail.com>
2015-10-21 09:31:58 -0700

    Fix memory leak

--- src/php/lib/Grpc/BaseStub.php
+++ src/php/lib/Grpc/BaseStub.php
@@ -114,9 +114,7 @@ class BaseStub {
       return true;
     }
     if ($new_state == \Grpc\CHANNEL_FATAL_FAILURE) {
-      // @codeCoverageIgnoreStart
       throw new \Exception('Failed to connect to server');
-      // @codeCoverageIgnoreEnd
     }
     return false;
   }
@@ -134,7 +132,7 @@ class BaseStub {
   private function _get_jwt_aud_uri($method) {
     $last_slash_idx = strrpos($method, '/');
     if ($last_slash_idx === false) {
-      throw new \InvalidArgumentException('service name must have a slash');
+      return false;
     }
     $service_name = substr($method, 0, $last_slash_idx);
     return "https://" . $this->hostname . $service_name;
