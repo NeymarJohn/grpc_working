da5f63f871ddf11d4ea0274e686267f9b2c6cc89
Sunriver <seriousman1994217winter@gmail.com>
2015-10-26 17:34:10 -0700

    update C# to the new C core auth API

--- src/node/src/metadata.js
+++ src/node/src/metadata.js
@@ -59,6 +59,7 @@ function normalizeKey(key) {
 function validate(key, value) {
   if (_.endsWith(key, '-bin')) {
     if (!(value instanceof Buffer)) {
+      console.log(value.constructor.toString());
       throw new Error('keys that end with \'-bin\' must have Buffer values');
     }
   } else {
