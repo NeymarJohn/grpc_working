a0ed47e9c568d72335f36b4247ed2a21c214572b
Sunriver <seriousman1994217winter@gmail.com>
2015-12-02 02:04:58 -0800

    Create Markdown table for structure and dev status

--- test/core/bad_client/bad_client.c
+++ test/core/bad_client/bad_client.c
@@ -64,13 +64,14 @@ static void done_write(grpc_exec_ctx *exec_ctx, void *arg, int success) {
   gpr_event_set(&a->done_write, (void *)1);
 }
 
-static void server_setup_transport(void *ts, grpc_transport *transport) {
+static void server_setup_transport(void *ts, grpc_transport *transport,
+                                   grpc_mdctx *mdctx) {
   thd_args *a = ts;
   static grpc_channel_filter const *extra_filters[] = {
       &grpc_http_server_filter};
   grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
   grpc_server_setup_transport(&exec_ctx, a->server, transport, extra_filters,
-                              GPR_ARRAY_SIZE(extra_filters),
+                              GPR_ARRAY_SIZE(extra_filters), mdctx,
                               grpc_server_get_channel_args(a->server));
   grpc_exec_ctx_finish(&exec_ctx);
 }
@@ -83,6 +84,7 @@ void grpc_run_bad_client_test(grpc_bad_client_server_side_validator validator,
   gpr_thd_id id;
   char *hex;
   grpc_transport *transport;
+  grpc_mdctx *mdctx = grpc_mdctx_create();
   gpr_slice slice =
       gpr_slice_from_copied_buffer(client_payload, client_payload_length);
   gpr_slice_buffer outgoing;
@@ -111,8 +113,9 @@ void grpc_run_bad_client_test(grpc_bad_client_server_side_validator validator,
   a.validator = validator;
   grpc_server_register_completion_queue(a.server, a.cq, NULL);
   grpc_server_start(a.server);
-  transport = grpc_create_chttp2_transport(&exec_ctx, NULL, sfd.server, 0);
-  server_setup_transport(&a, transport);
+  transport =
+      grpc_create_chttp2_transport(&exec_ctx, NULL, sfd.server, mdctx, 0);
+  server_setup_transport(&a, transport, mdctx);
   grpc_chttp2_transport_start_reading(&exec_ctx, transport, NULL, 0);
   grpc_exec_ctx_finish(&exec_ctx);
 
