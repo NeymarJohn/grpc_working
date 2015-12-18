cd6e55782911637f8e25aa1554ecdc8332611011
Sunriver <seriousman1994217winter@gmail.com>
2015-12-17 21:35:47 -0800

    Removing the peer from the SSL security connector.

--- test/cpp/end2end/server_crash_test_client.cc
+++ test/cpp/end2end/server_crash_test_client.cc
@@ -40,13 +40,13 @@
 #include <grpc++/channel.h>
 #include <grpc++/client_context.h>
 #include <grpc++/create_channel.h>
-#include "src/proto/grpc/testing/echo.grpc.pb.h"
+#include "test/cpp/util/echo.grpc.pb.h"
 
 DEFINE_string(address, "", "Address to connect to");
 DEFINE_string(mode, "", "Test mode to use");
 
-using grpc::testing::EchoRequest;
-using grpc::testing::EchoResponse;
+using grpc::cpp::test::util::EchoRequest;
+using grpc::cpp::test::util::EchoResponse;
 
 // In some distros, gflags is in the namespace google, and in some others,
 // in gflags. This hack is enabling us to find both.
@@ -57,7 +57,7 @@ using namespace gflags;
 
 int main(int argc, char** argv) {
   ParseCommandLineFlags(&argc, &argv, true);
-  auto stub = grpc::testing::TestService::NewStub(
+  auto stub = grpc::cpp::test::util::TestService::NewStub(
       grpc::CreateChannel(FLAGS_address, grpc::InsecureChannelCredentials()));
 
   EchoRequest request;
