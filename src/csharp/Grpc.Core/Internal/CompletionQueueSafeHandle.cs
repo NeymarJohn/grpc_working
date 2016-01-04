46429e942a1ace359da07b98b0fe72933e9d5747
Sunriver <seriousman1994217winter@gmail.com>
2016-01-04 15:34:44 -0800

    Update Ruby library version for next release

--- src/csharp/Grpc.Core/Internal/CompletionQueueSafeHandle.cs
+++ src/csharp/Grpc.Core/Internal/CompletionQueueSafeHandle.cs
@@ -33,8 +33,6 @@ using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Grpc.Core.Profiling;
 
-using Grpc.Core.Utils;
-
 namespace Grpc.Core.Internal
 {
     /// <summary>
@@ -42,8 +40,6 @@ namespace Grpc.Core.Internal
     /// </summary>
     internal class CompletionQueueSafeHandle : SafeHandleZeroIsInvalid
     {
-        AtomicCounter shutdownRefcount = new AtomicCounter(1);
-
         [DllImport("grpc_csharp_ext.dll")]
         static extern CompletionQueueSafeHandle grpcsharp_completion_queue_create();
 
@@ -66,7 +62,6 @@ namespace Grpc.Core.Internal
         public static CompletionQueueSafeHandle Create()
         {
             return grpcsharp_completion_queue_create();
-
         }
 
         public CompletionQueueEvent Next()
@@ -82,62 +77,15 @@ namespace Grpc.Core.Internal
             }
         }
 
-        /// <summary>
-        /// Creates a new usage scope for this completion queue. Once successfully created,
-        /// the completion queue won't be shutdown before scope.Dispose() is called.
-        /// </summary>
-        public UsageScope NewScope()
-        {
-            return new UsageScope(this);
-        }
-
         public void Shutdown()
         {
-            DecrementShutdownRefcount();
+            grpcsharp_completion_queue_shutdown(this);
         }
 
         protected override bool ReleaseHandle()
         {
             grpcsharp_completion_queue_destroy(handle);
             return true;
-        }
-
-        private void DecrementShutdownRefcount()
-        {
-            if (shutdownRefcount.Decrement() == 0)
-            {
-                grpcsharp_completion_queue_shutdown(this);
-            }
-        }
-
-        private void BeginOp()
-        {
-            bool success = false;
-            shutdownRefcount.IncrementIfNonzero(ref success);
-            Preconditions.CheckState(success, "Shutdown has already been called");
-        }
-
-        private void EndOp()
-        {
-            DecrementShutdownRefcount();
-        }
-
-        // Allows declaring BeginOp and EndOp of a completion queue with a using statement.
-        // Declared as struct for better performance.
-        public struct UsageScope : IDisposable
-        {
-            readonly CompletionQueueSafeHandle cq;
-
-            public UsageScope(CompletionQueueSafeHandle cq)
-            {
-                this.cq = cq;
-                this.cq.BeginOp();
-            }
-
-            public void Dispose()
-            {
-                cq.EndOp();
-            }
         }
     }
 }
