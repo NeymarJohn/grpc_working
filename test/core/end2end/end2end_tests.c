001270d621b142017aefc229de9ee26e4b3e671d
Sunriver <seriousman1994217winter@gmail.com>
2016-01-05 07:40:06 -0800

    Fix end2end_test formatting

--- test/core/end2end/end2end_tests.c
+++ test/core/end2end/end2end_tests.c
@@ -2,7 +2,7 @@
 
 /*
  *
- * Copyright 2015, Google Inc.
+ * Copyright 2015-2016, Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -32,6 +32,8 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  */
+
+
 
 /* This file is auto-generated */
 
@@ -269,3 +271,4 @@ void grpc_end2end_tests(int argc, char **argv,
     abort();
   }
 }
+
