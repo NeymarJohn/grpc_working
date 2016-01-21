95f7369bc120a11850faba3433825a5b5b1f041c
Sunriver <seriousman1994217winter@gmail.com>
2016-01-21 15:27:37 -0800

    Command line parsing fix

--- tools/run_tests/run_tests.py
+++ tools/run_tests/run_tests.py
@@ -471,7 +471,7 @@ class Sanity(object):
   def test_specs(self, config, args):
     import yaml
     with open('tools/run_tests/sanity_tests.yaml', 'r') as f:
-      return [config.job_spec([cmd['script']], None, timeout_seconds=None, environ={'TEST': 'true'}, cpu_cost=cmd.get('cpu_cost', 1))
+      return [config.job_spec(cmd['script'].split(), None, timeout_seconds=None, environ={'TEST': 'true'}, cpu_cost=cmd.get('cpu_cost', 1))
               for cmd in yaml.load(f)]
 
   def pre_build_steps(self):
@@ -820,7 +820,7 @@ else:
     if targets:
       return [jobset.JobSpec([os.getenv('MAKE', 'make'),
                               '-f', makefile,
-                              '-j', '%d' % args.jobs,
+                              '-j', '%d' % (multiprocessing.cpu_count() + 1),
                               'EXTRA_DEFINES=GRPC_TEST_SLOWDOWN_MACHINE_FACTOR=%f' % args.slowdown,
                               'CONFIG=%s' % cfg] +
                               language_make_options +
