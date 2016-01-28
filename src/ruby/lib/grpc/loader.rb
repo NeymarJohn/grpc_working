33f92a5862f60a74964bb987c5b3c4d64ecc6635
Sunriver <seriousman1994217winter@gmail.com>
2016-01-28 20:45:39 +0100

    Few more fixes to the Ruby installation.

--- src/ruby/lib/grpc/loader.rb
+++ src/ruby/lib/grpc/loader.rb
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Copyright 2015-2016, Google Inc.
 # All rights reserved.
 #
@@ -27,31 +26,9 @@
 # THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# This script is invoked by Jenkins and runs portability tests based on
-# env variable setting.
-#
-# Setting up rvm environment BEFORE we set -ex.
-[[ -s /etc/profile.d/rvm.sh ]] && . /etc/profile.d/rvm.sh
-# To prevent cygwin bash complaining about empty lines ending with \r
-# we set the igncr option. The option doesn't exist on Linux, so we fallback
-# to just 'set -ex' there.
-# NOTE: No empty lines should appear in this file before igncr is set!
-set -ex -o igncr || set -ex
 
-echo "building $scenario"
-
-parts=($(echo $scenario | tr '_' ' '))  # split scenario into parts
-
-curr_platform=${parts[0]}  # variable named 'platform' breaks the windows build
-curr_arch=${parts[1]}
-curr_compiler=${parts[2]}
-
-config='dbg'
-
-if [ "$curr_platform" == "linux" ]
-then
-  USE_DOCKER_MAYBE="--use_docker"
-fi
-
-python tools/run_tests/run_tests.py $USE_DOCKER_MAYBE -t -l $language -c $config --arch ${curr_arch} --compiler ${curr_compiler} -x report.xml -j 3 $@
+begin
+  require "grpc/#{RUBY_VERSION.sub(/\.\d$/, '')}/grpc"
+rescue LoadError
+  require 'grpc/grpc'
+end
