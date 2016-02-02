d61e4d7f3e2b8407d4043edc1ac3280f7f7d7d32
Sunriver <seriousman1994217winter@gmail.com>
2016-02-02 10:41:02 -0800

    C# centos docker target

--- test/cpp/qps/qps_worker.cc
+++ test/cpp/qps/qps_worker.cc
@@ -107,8 +107,8 @@ static std::unique_ptr<Server> CreateServer(const ServerConfig& config) {
 
 class WorkerServiceImpl GRPC_FINAL : public WorkerService::Service {
  public:
-  WorkerServiceImpl(int server_port, QpsWorker* worker)
-      : acquired_(false), server_port_(server_port), worker_(worker) {}
+  explicit WorkerServiceImpl(int server_port)
+      : acquired_(false), server_port_(server_port) {}
 
   Status RunClient(ServerContext* ctx,
                    ServerReaderWriter<ClientStatus, ClientArgs>* stream)
@@ -136,16 +136,6 @@ class WorkerServiceImpl GRPC_FINAL : public WorkerService::Service {
     Status ret = RunServerBody(ctx, stream);
     grpc_profiler_stop();
     return ret;
-  }
-
-  Status QuitWorker(ServerContext* ctx, const Void*, Void*) GRPC_OVERRIDE {
-    InstanceGuard g(this);
-    if (!g.Acquired()) {
-      return Status(StatusCode::RESOURCE_EXHAUSTED, "");
-    }
-
-    worker_->MarkDone();
-    return Status::OK;
   }
 
  private:
@@ -258,12 +248,10 @@ class WorkerServiceImpl GRPC_FINAL : public WorkerService::Service {
   std::mutex mu_;
   bool acquired_;
   int server_port_;
-  QpsWorker* worker_;
 };
 
 QpsWorker::QpsWorker(int driver_port, int server_port) {
-  impl_.reset(new WorkerServiceImpl(server_port, this));
-  gpr_atm_rel_store(&done_, static_cast<gpr_atm>(0));
+  impl_.reset(new WorkerServiceImpl(server_port));
 
   char* server_address = NULL;
   gpr_join_host_port(&server_address, "::", driver_port);
@@ -279,11 +267,5 @@ QpsWorker::QpsWorker(int driver_port, int server_port) {
 
 QpsWorker::~QpsWorker() {}
 
-bool QpsWorker::Done() const {
-  return (gpr_atm_acq_load(&done_) != static_cast<gpr_atm>(0));
-}
-void QpsWorker::MarkDone() {
-  gpr_atm_rel_store(&done_, static_cast<gpr_atm>(1));
-}
 }  // namespace testing
 }  // namespace grpc
