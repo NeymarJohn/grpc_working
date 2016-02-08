a576564a61e2c64c701972f4aa4fd8c8fd24a606
Sunriver <seriousman1994217winter@gmail.com>
2016-02-08 13:09:21 -0800

    Enable Node distrib tests on Mac

--- tools/run_tests/distribtest_targets.py
+++ tools/run_tests/distribtest_targets.py
@@ -106,15 +106,14 @@ class NodeDistribTest(object):
   """Tests Node package"""
 
   def __init__(self, platform, arch, docker_suffix, node_version):
-    self.name = 'node_npm_%s_%s_%s' % (platform, arch, node_version)
+    self.name = 'node_npm_%s_%s_%s_%s' % (platform, arch,
+                                          docker_suffix, node_version)
     self.platform = platform
     self.arch = arch
+    self.docker_suffix = docker_suffix
     self.node_version = node_version
     self.labels = ['distribtest', 'node', platform, arch,
                    docker_suffix, 'node-%s' % node_version]
-    if docker_suffix is not None:
-      self.name += '_%s' % docker_suffix
-      self.docker_suffix = docker_suffix
 
   def pre_build_jobspecs(self):
     return []
@@ -235,15 +234,10 @@ def targets():
           RubyDistribTest('linux', 'x64', 'ubuntu1504'),
           RubyDistribTest('linux', 'x64', 'ubuntu1510'),
           RubyDistribTest('linux', 'x64', 'ubuntu1604'),
-          NodeDistribTest('macos', 'x64', None, '0.10'),
-          NodeDistribTest('macos', 'x64', None, '0.12'),
-          NodeDistribTest('macos', 'x64', None, '3'),
-          NodeDistribTest('macos', 'x64', None, '4'),
-          NodeDistribTest('macos', 'x64', None, '5'),
           NodeDistribTest('linux', 'x86', 'jessie', '4')
           ] + [
             NodeDistribTest('linux', 'x64', os, version)
             for os in ('wheezy', 'jessie', 'ubuntu1204', 'ubuntu1404',
-                       'ubuntu1504', 'ubuntu1510', 'ubuntu1604')
+                       'ubuntu1504', 'ubuntu1510', 'ubuntu1604', 'macos')
             for version in ('0.10', '0.12', '3', '4', '5')
           ]
