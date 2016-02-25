d9c09e816bde56a39afbf90711503317438eeccb
Sunriver <seriousman1994217winter@gmail.com>
2016-02-25 13:55:13 -0800

    Change pollset mutex ownership

--- src/core/iomgr/pollset_set_posix.h
+++ src/core/iomgr/pollset_set_posix.h
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2015, Google Inc.
+ * Copyright 2015-2016, Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,6 @@
 #define GRPC_INTERNAL_CORE_IOMGR_POLLSET_SET_POSIX_H
 
 #include "src/core/iomgr/fd_posix.h"
-#include "src/core/iomgr/pollset_posix.h"
 
 typedef struct grpc_pollset_set {
   gpr_mu mu;
