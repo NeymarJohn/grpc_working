508a3a5e7972edbe4c283e40362c42184e3489f3
Sunriver <seriousman1994217winter@gmail.com>
2016-03-04 14:54:10 -0800

    Fix race between parsing messages and receiving status in Node client

--- src/core/iomgr/pollset_multipoller_with_poll_posix.c
+++ src/core/iomgr/pollset_multipoller_with_poll_posix.c
@@ -122,7 +122,6 @@ static void multipoll_with_poll_pollset_maybe_work_and_unlock(
     } else {
       h->fds[fd_count++] = h->fds[i];
       watchers[pfd_count].fd = h->fds[i];
-      GRPC_FD_REF(watchers[pfd_count].fd, "multipoller_start");
       pfds[pfd_count].fd = h->fds[i]->fd;
       pfds[pfd_count].revents = 0;
       pfd_count++;
@@ -136,10 +135,8 @@ static void multipoll_with_poll_pollset_maybe_work_and_unlock(
   gpr_mu_unlock(&pollset->mu);
 
   for (i = 2; i < pfd_count; i++) {
-    grpc_fd *fd = watchers[i].fd;
-    pfds[i].events = (short)grpc_fd_begin_poll(fd, pollset, worker,
+    pfds[i].events = (short)grpc_fd_begin_poll(watchers[i].fd, pollset, worker,
                                                POLLIN, POLLOUT, &watchers[i]);
-    GRPC_FD_UNREF(fd, "multipoller_start");
   }
 
   /* TODO(vpai): Consider first doing a 0 timeout poll here to avoid
--- tools/run_tests/post_test_node.bat
+++ tools/run_tests/post_test_node.bat
@@ -27,16 +27,4 @@
 @rem (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 @rem OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-set PATH=%PATH%;C:\Program Files\nodejs\;%APPDATA%\npm
-
-del /f /q BUILD || rmdir build /s /q
-
-call npm install --build-from-source
-
-@rem delete the redundant openssl headers
-for /f "delims=v" %%v in ('node --version') do (
-  rmdir "%HOMEDRIVE%%HOMEPATH%\.node-gyp\%%v\include\node\openssl" /S /Q
-)
-
-@rem rebuild, because it probably failed the first time
-call npm install --build-from-source
+rmdir node_modules /S /Q
