4db884eb3204541e2d4bade43752de395f617dcf
Sunriver <seriousman1994217winter@gmail.com>
2016-03-17 17:37:35 -0400

    move unix socket functionality to unix_sockets_posix module

--- src/cpp/codegen/grpc_library.cc
+++ src/cpp/codegen/grpc_library.cc
@@ -31,35 +31,10 @@
  *
  */
 
-#ifndef GRPC_INTERNAL_CORE_SUPPORT_BACKOFF_H
-#define GRPC_INTERNAL_CORE_SUPPORT_BACKOFF_H
+#include <grpc++/impl/codegen/grpc_library.h>
 
-#include <grpc/support/time.h>
+namespace grpc {
 
-typedef struct {
-  /// const: multiplier between retry attempts
-  double multiplier;
-  /// const: amount to randomize backoffs
-  double jitter;
-  /// const: minimum time between retries in milliseconds
-  int64_t min_timeout_millis;
-  /// const: maximum time between retries in milliseconds
-  int64_t max_timeout_millis;
+GrpcLibraryInterface *g_glip = nullptr;
 
-  /// random number generator
-  uint32_t rng_state;
-
-  /// current retry timeout in milliseconds
-  int64_t current_timeout_millis;
-} gpr_backoff;
-
-/// Initialize backoff machinery - does not need to be destroyed
-void gpr_backoff_init(gpr_backoff *backoff, double multiplier, double jitter,
-                      int64_t min_timeout_millis, int64_t max_timeout_millis);
-
-/// Begin retry loop: returns a timespec for the NEXT retry
-gpr_timespec gpr_backoff_begin(gpr_backoff *backoff, gpr_timespec now);
-/// Step a retry loop: returns a timespec for the NEXT retry
-gpr_timespec gpr_backoff_step(gpr_backoff *backoff, gpr_timespec now);
-
-#endif  // GRPC_INTERNAL_CORE_SUPPORT_BACKOFF_H
+}  // namespace grpc
