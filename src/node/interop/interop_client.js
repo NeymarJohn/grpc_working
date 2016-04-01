dcb59e882589f1cc65faa33cd41c925cd3604ff2
Sunriver <seriousman1994217winter@gmail.com>
2016-04-01 21:38:28 +0200

    Fuzzers now handle asserts.

--- src/node/interop/interop_client.js
+++ src/node/interop/interop_client.js
@@ -545,8 +545,6 @@ var test_cases = {
                   Client: testProto.TestService}
 };
 
-exports.test_cases = test_cases;
-
 /**
  * Execute a single test case.
  * @param {string} address The address of the server to connect to, in the
